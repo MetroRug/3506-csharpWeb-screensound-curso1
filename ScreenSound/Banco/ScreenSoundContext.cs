fc1c175b68486917268616db26dd95707a085c04
Lucas Santana Magalhães <74714365+MetroRug@users.noreply.github.com>
2025-03-24 19:41:31 -0300

    Conclusão do projeto

--- ScreenSound/Banco/ScreenSoundContext.cs
+++ ScreenSound/Banco/ScreenSoundContext.cs
@@ -1,27 +1,24 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using ScreenSound.Modelos;
-using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static System.Net.Mime.MediaTypeNames;
 
-namespace ScreenSound.Banco
+namespace ScreenSound.Banco;
+internal class ScreenSoundContext: DbContext
 {
-    class ScreenSoundContext: DbContext
-    {
-        public DbSet<Artista> Artistas { get; set; }
-        public DbSet<Musica> Musicas { get; set; }
+    public DbSet<Artista> Artistas { get; set; }
+    public DbSet<Musica> Musicas { get; set; }
 
-        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;Encrypt=False;TrustServerCertificate=False";
-      
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(connectionString);
-        }
+    private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSoundV0;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            .UseSqlServer(connectionString)
+            .UseLazyLoadingProxies(); 
     }
 }
